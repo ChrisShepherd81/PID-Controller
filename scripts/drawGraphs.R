28c093af2ed340befc61596fc983cf2d18fa1d0c
Christian <christian@inf-schaefer.de>
2017-05-28 11:19:49 +0200

    Bugfixing

--- scripts/drawGraphs.R
+++ scripts/drawGraphs.R
@@ -2,15 +2,25 @@
 args = commandArgs(trailingOnly=TRUE)
 
 if (length(args)==0) {
-  stop("Input csv file must be supplied", call.=FALSE)
+  stop("Usage: drawGraphs.R csv-file", call.=FALSE)
 } 
 
-csv <- read.csv(file=args[1], head=TRUE,sep=",")
-png('plot.png')
 samples = 500
-cte = head(csv[,2],samples)
-time = head(csv[,1],samples)
+
+csv <- read.csv(file=args[1], head=TRUE, sep=",")
+png(paste('plot', format(Sys.time(), "%d_%b_%Y-%H-%M-%S"), '.png', sep=""))
+
+#Read timestamp and cte value from csv file
+time = as.numeric(as.character(csv[1:samples+1,1])) 
+time = time - time[1]
+cte = c(csv[1:samples+1,2])
+
 plot(time, cte, type="l")
 lines(c(0,max(time)), c(0,0), col="red")
+title("CTE graph")
+# Add text using the following Corner_text function:
+Corner_text <- function(text, location="topright"){
+legend(location,legend=text, bty ="n", pch=NA) }
+Corner_text(text=paste("Kp: ", toString(csv[1,2]), "\nKi: ",  toString(csv[1,4]), "\nKd: ",  toString(csv[1,6])))
 
 dev.off()
